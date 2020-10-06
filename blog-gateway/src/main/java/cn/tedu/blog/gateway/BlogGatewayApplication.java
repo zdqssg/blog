b6d8f971de00856c28d3a9208b206c45028e2bb0
zdqssg <zdq247209@163.com>
2020-10-07 02:38:42 +0800

    readme

---
+++ blog-gateway/src/main/java/cn/tedu/blog/gateway/BlogGatewayApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.blog.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogGatewayApplication.class, args);
+    }
+
+}
